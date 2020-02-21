fba2f6d0206531eb884da82f201735af0ce6d5d6
koutriskyriakos <61329143+koutriskyriakos@users.noreply.github.com>
2020-02-22 00:43:57 +0200

    Create askhsh1.py

---
+++ askhsh1.py
@@ -0,0 +1,31 @@
+lista = []
+max_words_list = []
+i = 1
+
+
+def afairei_fwnienta(c):
+    newstr = c
+    vowels = ('a', 'e', 'i', 'o', 'u')
+    for x in c.lower():
+        if x in vowels:
+            newstr = newstr.replace(x,"")
+
+    return newstr
+
+f = open('/test.txt' , 'r')
+data = f.read().split()
+f.close()
+
+for n in data: lista.append(n)                #create list 'lista'
+
+while i < 6 :
+  max_word = max(lista, key = len)            #find the biggest word in the list 'lista'
+  max_words_list.append(max_word)             #vazw ti megaluterh leksi sth lista 'max_words_list'
+  lista.remove(max_word)
+  i += 1
+
+
+for n in max_words_list:  
+  k = afairei_fwnienta(n)                      #afairw ta fwnhenta apo tis lekseis
+  print(k[::-1])                               #emfanizw tis lekseis tis listas anapoda
+
