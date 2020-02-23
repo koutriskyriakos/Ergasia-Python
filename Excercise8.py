ce98bd4376a9c9490dd228f85b4e4b1e4d752a1f
koutriskyriakos <61329143+koutriskyriakos@users.noreply.github.com>
2020-02-23 20:45:01 +0200

    Update and rename Askhsh8.py to Excercise8.py

--- Excercise8.py
+++ Excercise8.py
@@ -2,19 +2,16 @@
 
 import random 
 
-light_one = random.randint(1, 10)
+light_one = random.randint(1, 10)                                   #dinei tuxaies times apo 1-10 sth metavliti
 light_two = random.randint(1, 10)
 light_three = random.randint(1, 10)
 value1 = random.randint(0, 5)
 value2 = random.randint(5, 10)
 value3 = random.randint(5, 10)
 
-if light_one > light_two and light_one > light_three: 
-  print(light_one)
-  print(light_two)
-  print(light_three)
-  light_one = light_one - value1
-  light_two = light_two + value2
+if light_one > light_two and light_one > light_three:               #elegxos gia to pio fanari exei perissotera autokinhta wste na to kanoume prasino
+  light_one = light_one - value1                                    #afairw mia tuxaia timi apo to prasino fanari gia na dw posa amaksia tha fugoun
+  light_two = light_two + value2                                    #prosthetw mia tuxaia timi sta kokkina fanaria gia na dw posa amaksia tha erthoun 
   light_three = light_three + value3
   print('There are', light_one, 'cars at the green light')
   print('There are', light_two, 'cars at the first red light')
@@ -24,9 +21,6 @@ if light_one > light_two and light_one > light_three:
   print(value3, 'cars came at the second red light')
   
 elif light_two > light_one and light_two > light_three: 
-  print(light_one)
-  print(light_two)
-  print(light_three)
   light_two = light_two - value1
   light_one = light_one + value2
   light_three = light_three + value3
@@ -38,9 +32,6 @@ elif light_two > light_one and light_two > light_three:
   print(value3, 'cars came at the second red light')
 
 elif light_three > light_two and light_three > light_one: 
-  print(light_one)
-  print(light_two)
-  print(light_three)
   light_three = light_three - value1
   light_two = light_two + value2
   light_one = light_one + value3
